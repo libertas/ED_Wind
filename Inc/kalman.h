62b55eb31490827d729d2cc7c29646a6b2be95ce
libertas <horizonvei@gmail.com>
2017-07-24 08:43:21 +0800

    structurize kalman

--- Inc/kalman.h
+++ Inc/kalman.h
@@ -8,6 +8,23 @@
 #ifndef KALMAN_H_
 #define KALMAN_H_
 
+typedef struct {
+	float K1;
+	float angle, angle_dot;
+	float Q_angle;// 过程噪声的协方差
+	float Q_gyro;//0.003 过程噪声的协方差 过程噪声的协方差为一个一行两列矩阵
+	float R_angle;// 测量噪声的协方差 既测量偏差
+	float dt;//
+	char  C_0;
+	float Q_bias, Angle_err;
+	float PCt_0, PCt_1, E;
+	float K_0, K_1, t_0, t_1;
+	float Pdot[4];
+	float PP[2][2];
+} kalman_t;
+
+void kalman_init(kalman_t *kt);
+void kalman_filter(kalman_t *kt, float Accel, float Gyro);
 
 
 #endif /* KALMAN_H_ */
--- Src/kalman.c
+++ Src/kalman.c
@@ -7,44 +7,48 @@
 
 #include "kalman.h"
 
-
-float K1 =0.02;
-float angle, angle_dot;
-float Q_angle=0.001;// 过程噪声的协方差
-float Q_gyro=0.003;//0.003 过程噪声的协方差 过程噪声的协方差为一个一行两列矩阵
-float R_angle=0.5;// 测量噪声的协方差 既测量偏差
-float dt=0.005;//
-char  C_0 = 1;
-float Q_bias, Angle_err;
-float PCt_0, PCt_1, E;
-float K_0, K_1, t_0, t_1;
-float Pdot[4] ={0,0,0,0};
-float PP[2][2] = { { 1, 0 },{ 0, 1 } };
-
-void Kalman_Filter(float Accel, float Gyro)
+void kalman_init(kalman_t *kt)
 {
-	angle+=(Gyro - Q_bias) * dt; //先验估计
-	Pdot[0]=Q_angle - PP[0][1] - PP[1][0]; // Pk-先验估计误差协方差的微分
-	Pdot[1]=-PP[1][1];
-	Pdot[2]=-PP[1][1];
-	Pdot[3]=Q_gyro;
-	PP[0][0] += Pdot[0] * dt;   // Pk-先验估计误差协方差微分的积分
-	PP[0][1] += Pdot[1] * dt;   // =先验估计误差协方差
-	PP[1][0] += Pdot[2] * dt;
-	PP[1][1] += Pdot[3] * dt;
-	Angle_err = Accel - angle;	//zk-先验估计
-	PCt_0 = C_0 * PP[0][0];
-	PCt_1 = C_0 * PP[1][0];
-	E = R_angle + C_0 * PCt_0;
-	K_0 = PCt_0 / E;
-	K_1 = PCt_1 / E;
-	t_0 = PCt_0;
-	t_1 = C_0 * PP[0][1];
-	PP[0][0] -= K_0 * t_0;		 //后验估计误差协方差
-	PP[0][1] -= K_0 * t_1;
-	PP[1][0] -= K_1 * t_0;
-	PP[1][1] -= K_1 * t_1;
-	angle	+= K_0 * Angle_err;	 //后验估计
-	Q_bias	+= K_1 * Angle_err;	 //后验估计
-	angle_dot   = Gyro - Q_bias;	 //输出值(后验估计)的微分=角速度
+	kt->K1 = 0.02;
+	kt->Q_angle = 0.001;
+	kt->Q_gyro = 0.003;
+	kt->R_angle = 0.5;
+	kt->dt = 0.005;
+	kt->C_0 = 1;
+	kt->Pdot[0] = 0;
+	kt->Pdot[1] = 0;
+	kt->Pdot[2] = 0;
+	kt->Pdot[3] = 0;
+	kt->PP[0][0] = 1;
+	kt->PP[0][1] = 0;
+	kt->PP[1][0] = 0;
+	kt->PP[1][1] = 1;
+}
+
+void kalman_filter(kalman_t *kt, float Accel, float Gyro)
+{
+	kt->angle += (Gyro - kt->Q_bias) * kt->dt; //先验估计
+	kt->Pdot[0] = kt->Q_angle - kt->PP[0][1] - kt->PP[1][0]; // Pk-先验估计误差协方差的微分
+	kt->Pdot[1] = -kt->PP[1][1];
+	kt->Pdot[2] = -kt->PP[1][1];
+	kt->Pdot[3] = kt->Q_gyro;
+	kt->PP[0][0] += kt->Pdot[0] * kt->dt;   // Pk-先验估计误差协方差微分的积分
+	kt->PP[0][1] += kt->Pdot[1] * kt->dt;   // =先验估计误差协方差
+	kt->PP[1][0] += kt->Pdot[2] * kt->dt;
+	kt->PP[1][1] += kt->Pdot[3] * kt->dt;
+	kt->Angle_err = Accel - kt->angle;	//zk-先验估计
+	kt->PCt_0 = kt->C_0 * kt->PP[0][0];
+	kt->PCt_1 = kt->C_0 * kt->PP[1][0];
+	kt->E = kt->R_angle + kt->C_0 * kt->PCt_0;
+	kt->K_0 = kt->PCt_0 / kt->E;
+	kt->K_1 = kt->PCt_1 / kt->E;
+	kt->t_0 = kt->PCt_0;
+	kt->t_1 = kt->C_0 * kt->PP[0][1];
+	kt->PP[0][0] -=kt-> K_0 * kt->t_0;		 //后验估计误差协方差
+	kt->PP[0][1] -= kt->K_0 * kt->t_1;
+	kt->PP[1][0] -= kt->K_1 * kt->t_0;
+	kt->PP[1][1] -= kt->K_1 * kt->t_1;
+	kt->angle	+= kt->K_0 * kt->Angle_err;	 //后验估计
+	kt->Q_bias	+= kt->K_1 * kt->Angle_err;	 //后验估计
+	kt->angle_dot   = Gyro - kt->Q_bias;	 //输出值(后验估计)的微分=角速度
 }
